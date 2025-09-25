c3821989682adb87b77f5eed4e7b7b2bcc9cef3d
Johan M <penguin_gtr@hotmail.com>
2025-09-25 11:21:32 +0200

    Added first project
    added program that converts seconds to dhms

---
+++ secondsConv/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter amount of seconds");
+Console.WriteLine();
+int secondsInput = int.Parse(Console.ReadLine());
+double dseconds = Convert.ToDouble(secondsInput);
+int restSeconds = secondsInput % 60;
+int minutes = secondsInput / 60;
+
+int restMinutes = minutes % 60;
+int hours = minutes / 60;
+
+int restHours = hours % 24;
+int days = hours / 24;
+double ddays = dseconds / 86400;
+Console.WriteLine($"seconds: {restSeconds}");
+Console.WriteLine($"Minutes: {restMinutes}");
+Console.WriteLine($"Hours: {restHours}");
+Console.WriteLine($"Days: {days}");
+Console.WriteLine($"D{days}.H{restHours}:M{restMinutes}:S{restSeconds}");
+Console.WriteLine($"That is {ddays} days in total");
